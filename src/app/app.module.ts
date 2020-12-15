502fc69d73680b124e1b7397f5b84afadd38f101
kbrv37 <Al.pizano@motorolasolutions.com>
2020-12-15 17:25:17 -0600

    adding test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
