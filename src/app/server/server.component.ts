85e6fc415899e04e9257b85f8fd6a70123da49d0
alpizano <apizano@purdue.edu>
2020-12-22 19:23:47 -0600

    string interpolation, property binding and event biding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,35 +1,40 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
 })
-
 export class ServerComponent {
-    serverId = 10;
-    serverStatus: string = 'offline';
-    allowNewServer = false;
-    serverCreationStatus = "No server was created!"
+  serverId = 10;
+  serverStatus: string = 'offline';
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
-    // execute at time component is created by angular
-    constructor() {
-        setTimeout( () => {
-            this.allowNewServer = true;
-        },2000);
-    }
+  // execute at time component is created by angular
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
-    getServerStatus() {
-        return this.serverStatus;
-    }
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
-    onCreateServer() {
-        this.serverCreationStatus = "Server was created!"
-    }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 
-    // log events of the dom element
-    log(event) {
-        const button = event.target;
-        console.log(button);
-        console.dir(button);
-    }
+  onUpdateServerName(event: any) {
+    //this.serverName = (<HTMLInputElement>event.target).value;
+    this.serverName = event.target.value;
+  }
+
+  // log events of the dom element
+  log(event) {
+    const button = event.target;
+    console.log(button);
+    console.dir(button);
+  }
 }
