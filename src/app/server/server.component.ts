8de7dae7bd486ed614f5c26ff9f79f141e4e5703
alpizano <apizano@purdue.edu>
2020-12-27 22:47:35 -0600

    refactoring server component to use directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -14,18 +14,10 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   serverId = 10;
   serverStatus: string = 'offline';
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created!';
-  serverName = 'Testserver';
-  userName = '';
-  serverCreated = false;
 
   // execute at time component is created by angular
   constructor() {
     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
   }
 
   getServerStatus() {
@@ -34,40 +26,5 @@ export class ServerComponent {
 
   getColor() {
     return this.serverStatus === 'online' ? 'green' : 'red';
-  }
-
-  // when AddServer button is clicked
-  onCreateServer() {
-    this.serverCreated = true;
-    this.serverCreationStatus = 'Server was created!';
-  }
-
-  // implementing updating username but with event binding
-  onUpdateUserName(event: any) {
-    this.userName = event.target.value;
-  }
-
-  // should only be clickable () if not empty string
-  isUserNameNull(): boolean {
-    if (this.userName !== '') {
-      return false;
-    }
-    return true;
-  }
-
-  resetUserName(): void {
-    this.userName = '';
-  }
-
-  onUpdateServerName(event: any) {
-    //this.serverName = (<HTMLInputElement>event.target).value;
-    this.serverName = event.target.value;
-  }
-
-  // log events of the dom element
-  log(event) {
-    const button = event.target;
-    console.log(button);
-    console.dir(button);
   }
 }
