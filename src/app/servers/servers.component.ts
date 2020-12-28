e3446af4f19f0e89de4efa5ad027323afbeb7c8d
alpizano <apizano@purdue.edu>
2020-12-27 22:47:59 -0600

    added servers component and nested server component dynamically using ngFor directive

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  serverId = 10;
+  serverStatus: string = 'offline';
+  allowNewServer = true;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  userName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+
+  // execute at time component is created by angular
+  constructor() {
+    // becomes clickable only after 2secs
+    // setTimeout(() => {
+    //   this.allowNewServer = true;
+    // }, 2000);
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+  // when AddServer button is clicked
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  // implementing updating username but with event binding
+  onUpdateUserName(event: any) {
+    this.userName = event.target.value;
+  }
+
+  // should only be clickable () if not empty string
+  isUserNameNull(): boolean {
+    if (this.userName !== '') {
+      return false;
+    }
+    return true;
+  }
+
+  resetUserName(): void {
+    this.userName = '';
+  }
+
+  onUpdateServerName(event: any) {
+    //this.serverName = (<HTMLInputElement>event.target).value;
+    this.serverName = event.target.value;
+  }
+
+  // log events of the dom element
+  log(event) {
+    const button = event.target;
+    console.log(button);
+    console.dir(button);
+  }
+
+  ngOnInit(): void {}
+}
